0bca07516238717457696b6ea4df0fd294fa194a
AdrieanKhisbe <adriean.khisbe@live.fr>
2017-04-02 00:05:35 +0200

    build: fix publish coverage broken since the suppression of cucumber

--- .coverage.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mkdir -p coverage
-lab -r lcov -o coverage/lab.lcov -r html -o coverage/index.html
-istanbul cover cucumber.js --lcovonly -- --tags ~@ignore --format json > /dev/null
-lcov-result-merger 'coverage/*.lcov' 'coverage/merged.lcov'
