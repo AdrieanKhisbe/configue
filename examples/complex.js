a3dae5002eeef9d4dc5eaa3339f62c2d86cde193
AdrieanKhisbe <adriean.khisbe@live.fr>
2017-10-27 21:42:33 +0200

    feature: template string improved with default values

--- examples/complex.js
+++ examples/complex.js
@@ -3,8 +3,4 @@
 const Configue = require('configue');
 const configue = Configue(require('./complex-configue'));
 
-const who = configue.get('who', 'World');
-const salute = configue.get('salute');
-const times = configue.get('times');
-
-console.log(`I will say ${salute} to ${who} ${times} times`);
+console.log(configue.t({who:'World'})`I will say ${'salute'} to ${'who'} ${'times'} times`);
